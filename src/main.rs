1a1f9dc4d080e70273ae88c4033d155cc7b6fadc
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-02-05 11:17:25 +0900

    create Deck struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Deck {
+    cards: Vec<i32>,
+}
+impl Deck {
+    fn new () -> Deck {
+        Deck {
+            cards: vec![],
+        }
+    }
+    fn add(&mut self) {
+        let mut _case = vec![
+            1, 2, 3, 4, 5, 6, 7,
+            8, 9, 10, 10, 10, 10
+            ];
+        self.cards = _case;
+    }
+}
+
 fn main() {
+    let mut deck = Deck::new();
+    deck.add();
+
+    print_cards(deck.cards);
     println!("Hello, world!");
+}
+fn print_cards(_cards: Vec<i32>) {
+    for card in _cards {
+        print!("{}, ", card);
+    }
+    println!("");
 }
