e941b952bf7d333188f43d1ce1b63863060cbfcc
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-02-06 10:07:25 +0900

    add 'add' of Deck struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::seq::SliceRandom;
+
 struct Deck {
     cards: Vec<i32>,
 }
@@ -9,9 +11,12 @@ impl Deck {
     }
     fn add(&mut self) {
         let mut _case = vec![
-            1, 2, 3, 4, 5, 6, 7,
-            8, 9, 10, 10, 10, 10
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
             ];
+        _case.shuffle(&mut rand::thread_rng());
         self.cards = _case;
     }
 }
@@ -20,12 +25,15 @@ fn main() {
     let mut deck = Deck::new();
     deck.add();
 
-    print_cards(deck.cards);
+    println!("{}", deck.cards.len());
+    println!("{:?}", deck.cards);
     println!("Hello, world!");
 }
-fn print_cards(_cards: Vec<i32>) {
-    for card in _cards {
-        print!("{}, ", card);
-    }
-    println!("");
-}
+
+// == println!("{:?}", _cards);
+// fn print_cards(_cards: Vec<i32>) {
+//     for card in _cards {
+//         print!("{}, ", card);
+//     }
+//     println!("");
+// }
